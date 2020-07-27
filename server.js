bdc7454fbfe048dbd2aa42cc131716d34968ca0a
Brad Pritchett <bradpritchett1@msn.com>
2020-07-27 15:37:33 -0600

    setting up server

--- server.js
+++ server.js
@@ -3,3 +3,11 @@ var path = require("path");
 const {
     table
 } = require("console");
+
+var app = express();
+var PORT = 8080;
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+app.use(express.static('public'))
